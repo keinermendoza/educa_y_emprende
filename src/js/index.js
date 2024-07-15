6fcd72b836dfdb18de69107a4e70f03a1a8d5c1d
keiner <keinermendoza@gmail.com>
2024-07-15 19:22:25 -0400

    adding container queries for brand cards

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,12 @@
-import Alpine from 'alpinejs';
 import Swiper from 'swiper';
 import { Autoplay, Navigation, Pagination, EffectCards } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/effect-cards';
 import { register as swipperRegister  } from 'swiper/element/bundle';
 
-window.htmx = require('htmx.org');
 
 document.addEventListener("DOMContentLoaded", () => {
-    window.Alpine = Alpine;
-    Alpine.start();
+    
 
     swipperRegister();
 });
---
+++ src/js/vendor.js
@@ -0,0 +1,5 @@
+import Alpine from 'alpinejs';
+import 'container-query-polyfill'
+window.htmx = require('htmx.org');
+window.Alpine = Alpine;
+Alpine.start();
