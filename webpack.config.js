72310ae6c0edb6092fb0a14441f171b707ca38af
keiner <keinermendoza@gmail.com>
2024-07-15 16:17:40 -0400

    working in about section

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     entry: {
-        index: './src/index.js',
+        index: './src/js/index.js',
         // editor_app:'./src/react/editor_app.jsx',
     },
     output: {
